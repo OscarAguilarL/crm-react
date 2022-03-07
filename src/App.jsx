0ed481b0aa38c049e90323fe0f4ff136d6c1aef9
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-03-06 20:38:58 -0600

    Agrupando Rutas y Definiendo Layout Principal con React Router 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { IniciarSesion } from './layout/IniciarSesion';
+
 import { Layout } from './layout/Layout';
+import { EditarCliente } from './pages/EditarCliente';
 import { Inicio } from './pages/Inicio';
-import { LoginForm } from './pages/LoginForm';
+import { NuevoCliente } from './pages/NuevoCiente';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<IniciarSesion />}>
-          <Route index element={<LoginForm />} />
-        </Route>
-
         <Route path="/clientes" element={<Layout />}>
           <Route index element={<Inicio />} />
+          <Route path="nuevo" element={<NuevoCliente />} />
+          <Route path="editar/:id" element={<EditarCliente />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/LoginForm.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const LoginForm = () => {
-  return (
-    <div>
-      <h1>Fromulario</h1>
-    </div>
-  )
-}
